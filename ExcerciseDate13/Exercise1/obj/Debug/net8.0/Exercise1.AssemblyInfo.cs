e80de30fcb00c506d64cfc1e5f6f17bae675f340
huetruong <truongvshieu01@gmail.com>
2024-11-30 10:50:25 +0700

    exerscise18

--- ExcerciseDate13/Exercise1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
+++ ExcerciseDate13/Exercise1/obj/Debug/net8.0/Exercise1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b413380ebb99036ac58ea92d0c85e1d3a572a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
