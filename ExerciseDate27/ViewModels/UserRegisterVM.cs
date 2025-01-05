d0a64c8317de0d75c0513682ebd63fb653827d0f
huetruong <truongvshieu01@gmail.com>
2025-01-05 21:09:38 +0700

    exercise date27

---
+++ ExerciseDate27/ViewModels/UserRegisterVM.cs
@@ -0,0 +1,18 @@
+namespace Exercise27.ViewModels;
+class UserRegiserVM {
+  public string FullName { get; set; }
+
+  public string Email { get; set; }
+
+  public string PhoneNumber { get; set; }
+
+  public string Address { get; set; }
+
+  public string Message { get; set; }
+
+  public string Gender { get; set; }
+
+  public string SelectService { get; set; }
+
+  public bool TermAndConditions {get; set;}
+}
