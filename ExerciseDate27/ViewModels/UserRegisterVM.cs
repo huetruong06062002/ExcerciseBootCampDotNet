16034c8f6b674c89f10a860f8b519867c64e0c26
huetruong <truongvshieu01@gmail.com>
2025-01-07 09:28:21 +0700

    exercise2date27

--- ExerciseDate27/ViewModels/UserRegisterVM.cs
+++ ExerciseDate27/ViewModels/UserRegisterVM.cs
@@ -1,18 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Exercise27.ViewModels;
-class UserRegiserVM {
+class UserRegiserVM
+{
+  [Required(ErrorMessage = "FullName is required")]
+  [MinLength(3, ErrorMessage = "FullName must be at least 3 characters")]
   public string FullName { get; set; }
 
+  [Required(ErrorMessage = "Email is required")]
+  [EmailAddress(ErrorMessage = "Invalid email format.")]
+  [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email is not valid.")]
   public string Email { get; set; }
 
+  [Required(ErrorMessage = "Phone Number is required.")]
+  [RegularExpression(@"^\d{10,12}$", ErrorMessage = "Phone number must be 10-12 digits.")]
   public string PhoneNumber { get; set; }
 
-  public string Address { get; set; }
+  [Required(ErrorMessage = "Please select a course.")]
+  public string SelectedCourse {get; set;}
 
-  public string Message { get; set; }
+  [Required(ErrorMessage ="Please select a study mode.")]
+  public string StudyMode {get; set;}
 
-  public string Gender { get; set; }
+  [Required(ErrorMessage = "Preferred start date is required.")]
+  public DateTime? PreferredStartDate {get; set;}
 
-  public string SelectService { get; set; }
+  public string Comments{get; set;}
 
-  public bool TermAndConditions {get; set;}
+  [Required(ErrorMessage = "You must agree to the terms and conditions.")]
+  public bool AgreeToTerms {get; set;}
 }
--- ExerciseDate27/obj/Debug/net7.0/Exercise27.AssemblyInfo.cs
+++ ExerciseDate27/obj/Debug/net7.0/Exercise27.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercise27")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b4e53bd60eec56ac712b93804c5b67a22d4e9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercise27")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercise27")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
